bfaefefeffd2f4b816e2faed9dfc446373140aef
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2022-04-07 10:46:48 +0700

    fix: always recreate finder class

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -2,11 +2,15 @@
 
 namespace Haemanthus\CodeIgniter3IdeHelper\Providers;
 
+use DI\Scope;
+use Symfony\Component\Finder\Finder;
+
 class AppServiceProvider
 {
     public static function register()
     {
         return [
+            Finder::class => \DI\object(Finder::class)->scope(Scope::PROTOTYPE),
         ];
     }
 }
