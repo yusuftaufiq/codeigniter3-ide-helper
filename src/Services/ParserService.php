54cc96eeba0cdaacef43af0dab99aa09636fd1c6
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2022-04-11 14:34:51 +0700

    feat: create parser service blueprint

--- src/Services/ParserService.php
+++ src/Services/ParserService.php
@@ -4,4 +4,25 @@ namespace Haemanthus\CodeIgniter3IdeHelper\Services;
 
 class ParserService
 {
+    public $autoloadParser;
+
+    public $coreParser;
+
+    public function __construct(
+        $autoloadParser,
+        $coreParser
+    ) {
+        $this->autoloadParser = $autoloadParser;
+        $this->coreParser = $coreParser;
+    }
+
+    public function parseAutoload(string $content)
+    {
+        $this->autoloadParser->parse($content);
+    }
+
+    public function parseCore(string $content)
+    {
+        $this->coreParser->parse($content);
+    }
 }
