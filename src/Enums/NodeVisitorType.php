d442f45022590226efafda824ab1fc4ac141b422
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2022-05-20 10:19:14 +0700

    feat: add new node visitor type

--- src/Enums/NodeVisitorType.php
+++ src/Enums/NodeVisitorType.php
@@ -5,6 +5,7 @@ namespace Haemanthus\CodeIgniter3IdeHelper\Enums;
 use Spatie\Enum\Enum;
 
 /**
+ * @method static self class()
  * @method static self assignAutoloadLibrary()
  * @method static self assignAutoloadModel()
  * @method static self methodCallLoadLibrary()
